bac891605942a9917b7e4896b276e11b8ca2b750
Wei-bin-Wu <w656641296@gmail.com>
2021-05-21 13:04:34 +0200

    nota

--- DS6/app/controllers/nota_controller.rb
+++ DS6/app/controllers/nota_controller.rb
@@ -38,7 +38,7 @@ class NotaController < ApplicationController
   def update
     respond_to do |format|
       if @nota.update(nota_params)
-        format.html { redirect_to @nota, notice: "Notum was successfully updated." }
+        format.html { redirect_to @nota, notice: "Nota was successfully updated." }
         format.json { render :show, status: :ok, location: @nota }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,7 +51,7 @@ class NotaController < ApplicationController
   def destroy
     @nota.destroy
     respond_to do |format|
-      format.html { redirect_to nota_url, notice: "Notum was successfully destroyed." }
+      format.html { redirect_to nota_url, notice: "Nota was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -59,7 +59,7 @@ class NotaController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_nota
-      @nota = Notum.find(params[:id])
+      @nota = Nota.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
