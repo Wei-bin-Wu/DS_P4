fe2a20242cc66080ed3c91ed24165b9c58065f4b
Wei-bin-Wu <w656641296@gmail.com>
2021-05-23 23:19:46 +0200

    nota

--- DS6/db/migrate/20210523210953_create_nota.rb
+++ DS6/db/migrate/20210523210953_create_nota.rb
@@ -3,8 +3,8 @@ class CreateNota < ActiveRecord::Migration[6.1]
     create_table :nota do |t|
       t.belongs_to :alumnos, null: false, foreign_key: true
       t.belongs_to :examenes, null: false, foreign_key: true
-      t.integer :nota
-      t.string :foto
+      t.integer :nota, null:false, :in => 0..10
+      t.string :foto, :limit => 128
 
       t.timestamps
     end
