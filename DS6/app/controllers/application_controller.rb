bf4f4f706fc0c46b2ca31433f5563018b083f506
Wei-bin-Wu <w656641296@gmail.com>
2021-05-29 22:51:12 +0200

    api

--- DS6/app/controllers/application_controller.rb
+++ DS6/app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-    http_basic_authenticate_with name: "administrador", password: "administrador", except: [:index, :show, :descargar]
+    protect_from_forgery with: :exception
 end
