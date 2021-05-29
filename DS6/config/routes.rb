212bc5d3db0624692b28b0aa673cb410e3dcad5e
Wei-bin-Wu <w656641296@gmail.com>
2021-05-29 21:23:29 +0200

    rollback

--- DS6/config/routes.rb
+++ DS6/config/routes.rb
@@ -7,13 +7,9 @@ Rails.application.routes.draw do
   resources :students
   resources :professors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'sessions#login'
+  root to: 'professors#index'
 
   get "/app/assets/apks/flutter.apk" => "descarga#descargar"
   get "/app/assets/images/*route", to:"notes#descargar"
-  get "/login" => "sessions#login"
-  post "/login" => "sessions#create"
-  get "/logout" => "sessions#destroy"
-  post "/logout" => "sessions#destroy"
   
 end
