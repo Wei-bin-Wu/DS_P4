c1f47ba9a38911fda152dd550c47b4d3509432b2
Wei-bin-Wu <w656641296@gmail.com>
2021-05-29 23:35:03 +0200

    api

--- DS6/app/controllers/application_controller.rb
+++ DS6/app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show, :descargar]
 end
