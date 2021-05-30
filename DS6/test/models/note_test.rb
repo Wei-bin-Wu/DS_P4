94396861456905b174f423525dd1a31ca8d6a359
Wei-bin-Wu <w656641296@gmail.com>
2021-05-30 23:49:34 +0200

    test

--- DS6/test/models/note_test.rb
+++ DS6/test/models/note_test.rb
@@ -1,10 +1,14 @@
 require "test_helper"
 
 class NoteTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+  
   test "No debe guardar nota con campo student vacio" do
     nota = Note.new(exam:1, note:7, photo:"/imgs")
     assert_not nota.save, "Guardando nota con campo stundent vacío"
   end
 
-  
+
 end
--- DS6/test/models/professor_test.rb
+++ DS6/test/models/professor_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class ProfessorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "No debe guardar profesor con campo telefono con longitud mayor que 9" do
+    p = Professor.new(id: 3, name: "Daniel", surname: "Saucedo", phone:"1234567890", email: "daniel@gmail.com", pass: "daniel")
+    assert p.save, "Guardando correctamente"
+    assert_equal 9, p.phone.length
+  end
 end
