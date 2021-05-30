ee7f27da371928df78b4d797cbebb8f2c35b6285
Wei-bin-Wu <w656641296@gmail.com>
2021-05-31 00:27:30 +0200

    test

--- DS6/test/models/exam_test.rb
+++ DS6/test/models/exam_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class ExamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Introducir fecha con un formato erroneo" do
+    exam = Exam.new(name:"ex1", date:"12kj4hi1uhasd", subject:1)
+    assert_not exam.save, "Guardando examen con campo fecha no valido"
+  end
 end
