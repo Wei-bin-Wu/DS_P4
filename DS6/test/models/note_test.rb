705dddd3d46d2150c4c298dc0969c80f4c02e162
Wei-bin-Wu <w656641296@gmail.com>
2021-05-30 23:37:47 +0200

    test

--- DS6/test/models/note_test.rb
+++ DS6/test/models/note_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "No debe guardar nota con campo student vacio" do
+    nota = Note.new(exam:1, note:7, photo:"/imgs")
+    assert_not nota.save, "Guardando nota con campo stundent vacío"
+  end
+
+  
 end
